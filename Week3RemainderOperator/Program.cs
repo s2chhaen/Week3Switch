435da9524968164679b89b9b36606df5475b372f
Christian Haendel <example@example.com>
2023-12-14 19:10:05 +0200

    Week 3

---
+++ Week3RemainderOperator/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 11;
+
+int result = a % 2;
+Console.WriteLine(result);
+
+//if (result == 0)
+//{
+//    Console.WriteLine($"{a} is an even number.");
+//}
+//else
+//{
+//    Console.WriteLine($"{a} is an odd number.");
+//}
+
+switch (result)
+{
+	case 0:
+        Console.WriteLine($"{a} is an even number.");
+        break;
+    case 1:
+        Console.WriteLine($"{a} is an odd number.");
+        break;
+    default:
+        Console.WriteLine("Invalid value");
+        break;
+}
---
+++ Week3Switch/Program.cs
@@ -0,0 +1,46 @@
+﻿int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+Console.WriteLine(weekDay);
+
+switch (weekDay)
+{
+    case 0:
+        {
+            Console.WriteLine("Sunday");
+            break;
+        }
+    case 1:
+        {
+            Console.WriteLine("Monday");
+            break;
+        }
+    case 2:
+        {
+            Console.WriteLine("Tuesday");
+            break;
+        }
+    case 3:
+        {
+            Console.WriteLine("Wednesday");
+            break;
+        }
+    case 4:
+        {
+            Console.WriteLine("Thursday");
+            break;
+        }
+    case 5:
+        {
+            Console.WriteLine("Friday");
+            break;
+        }
+    case 6:
+        {
+            Console.WriteLine("Saturday");
+            break;
+        }
+    default:
+        {
+            Console.WriteLine("Invalid value");
+            break;
+        }
+}
